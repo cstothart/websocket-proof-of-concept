ede6a459ee0863dad598150236df01697d77c4b5
cstothart <cary.stothart@gmail.com>
2020-08-28 16:48:08 -0500

    Added name input, minimal name validation, and name array.

--- app.js
+++ app.js
@@ -8,14 +8,35 @@ app.use(express.static('public'));
 const server = app.listen(port);
 const io = require('socket.io')(server);
 
-io.on('connect', socket => {
+let players = [];
 
-  console.log('A client connected.');
-  socket.broadcast.emit('alert', 'Someone joined');
+io.on('connection', socket => {
+
+  let inGame = false;
+
+  socket.join('name room');
+
+  socket.on('received name', nameSubmitted => {
+    if(nameSubmitted == '') {
+      socket.emit('validation response', false);
+    } else {
+      socket.emit('validation response', true);
+      socket.name = nameSubmitted;
+      players.push(socket.name);
+      console.log(players);
+      socket.join('game room', () => {
+        inGame = true;
+        socket.to('game room').emit('alert', `${socket.name} joined`);
+      });
+    }
+  });
 
   socket.on('disconnect', () => {
-    console.log('A client disconnected.');
-    socket.broadcast.emit('alert', 'Someone left');
-  });
+    if(inGame) {
+      const index = players.indexOf(socket.name);
+      players.splice(index, 1);
+      socket.to('game room').emit('alert', `${socket.name} left`);
+    }
+  });  
 
 });
--- public/main.js
+++ public/main.js
@@ -3,15 +3,20 @@ const socket = io();
 const alertModal = document.querySelector('#alertModal');
 
 socket.on('alert', alertMsg => {
-
-  console.log(alertMsg);
-
-  alertModal.style.opacity = '1';
-  alertModal.textContent = alertMsg;
-
-  window.setTimeout(() => {
-    alertModal.style.opacity = '0';
-  }, 1010);
-
+    console.log(alertMsg);
+    alertModal.style.opacity = '1';
+    alertModal.textContent = alertMsg;
+    window.setTimeout(() => {
+      alertModal.style.opacity = '0';
+    }, 1010);
 });
 
+const handleName = () => {
+  const nameInput = document.querySelector('#nameField').value;
+  socket.emit('received name', nameInput);
+  socket.on('validation response', response => {
+    if(response) {
+      document.querySelector('#nameModal').style.display = 'none';
+    }
+  });
+}
