7dfbc8ab5ecf8fcf1e79a37ef745164ca314555e
cstothart <cary.stothart@gmail.com>
2020-09-03 13:45:18 -0500

    Added resource spawning and deletion.

--- app.js
+++ app.js
@@ -8,7 +8,30 @@ app.use(express.static('public'));
 const server = app.listen(port);
 const io = require('socket.io')(server);
 
-let players = [];
+const gameConfig = {
+  resources: {
+    maxNumber: 3,
+    // Max resource spawn distance from window center.
+    maxDistance: 200,
+    width: 20,
+    height: 20,
+    borderWidth: 2
+  }
+};
+
+const gameState = {
+  players: [],
+  resources: []
+}
+
+const deleteResource = resourceId => {
+  io.emit('delete resources', resourceId);
+  gameState.resources.forEach((resource, index) => {
+    if(resource.id === resourceId) {
+      gameState.resources.splice(index, 1);
+    }
+  });
+}
 
 io.on('connection', socket => {
 
@@ -22,21 +45,47 @@ io.on('connection', socket => {
     } else {
       socket.emit('validation response', true);
       socket.name = nameSubmitted;
-      players.push(socket.name);
-      console.log(players);
+      gameState.players.push(socket.name);
       socket.join('game room', () => {
         inGame = true;
         socket.to('game room').emit('alert', `${socket.name} joined`);
+        io.emit('player list', gameState.players);
+        socket.emit('add resources', gameState.resources);
       });
     }
   });
 
+  socket.on('resource click', resourceId => {
+    deleteResource(resourceId);
+  });
+
   socket.on('disconnect', () => {
     if(inGame) {
-      const index = players.indexOf(socket.name);
-      players.splice(index, 1);
+      const index = gameState.players.indexOf(socket.name);
+      gameState.players.splice(index, 1);
       socket.to('game room').emit('alert', `${socket.name} left`);
+      io.emit('player list', gameState.players);
     }
-  });  
+  });
 
 });
+
+setInterval(() => {
+  if(gameState.resources.length < gameConfig.resources.maxNumber) {
+    const resourceId = `r${Math.floor(Math.random()*1000000000).toString()}`;
+    const newResource = {
+      id: resourceId, 
+      pos: {
+        xFromCenter: Math.floor(Math.random()*gameConfig.resources.maxDistance),
+        yFromCenter: Math.floor(Math.random()*gameConfig.resources.maxDistance)
+      },
+      size: {
+        width: gameConfig.resources.width,
+        height: gameConfig.resources.height
+      },
+      borderWidth: gameConfig.resources.borderWidth
+    }
+    gameState.resources.push(newResource);
+    io.emit('add resources', [newResource]);
+  }
+}, 1);
--- public/main.js
+++ public/main.js
@@ -1,6 +1,10 @@
 const socket = io();
 
 const alertModal = document.querySelector('#alertModal');
+const nameScreen = document.querySelector('#nameScreen');
+const gameScreen = document.querySelector('#gameScreen');
+const playerModal = document.querySelector('#playerModal');
+const playerList = document.querySelector('#playerList');
 
 socket.on('alert', alertMsg => {
     console.log(alertMsg);
@@ -16,7 +20,54 @@ const handleName = () => {
   socket.emit('received name', nameInput);
   socket.on('validation response', response => {
     if(response) {
-      document.querySelector('#nameModal').style.display = 'none';
+      nameScreen.style.display = 'none';
+      gameScreen.style.display = 'flex';
     }
   });
 }
+
+socket.on('player list', players => {
+  playerList.textContent = '';
+  players.forEach(player => {
+    const para = document.createElement('li');
+    const text = document.createTextNode(player);
+    para.appendChild(text);
+    playerList.appendChild(para);
+  });
+});
+
+const handleResourceClick = rsrcId => {
+  socket.emit('resource click', rsrcId);
+}
+
+socket.on('add resources', resources => {
+  console.log(resources);
+  resources.forEach(rsrc => {
+    const rsrcX = (window.innerWidth/2) + 
+      rsrc.pos.xFromCenter - 
+      (rsrc.size.width/2) - 
+      rsrc.borderWidth;
+    const rsrcY = (window.innerHeight/2) + 
+      rsrc.pos.yFromCenter - 
+      (rsrc.size.height/2) - 
+      rsrc.borderWidth;
+    const resource = document.createElement('div');
+    resource.classList.add('resource');
+    resource.setAttribute('id', rsrc.id);
+    resource.style.height = `${rsrc.size.width}px`;
+    resource.style.width = `${rsrc.size.height}px`;
+    resource.style.borderWidth = `${rsrc.borderWidth}px`;
+    resource.style.left = `${rsrcX}px`;
+    resource.style.top = `${rsrcY}px`;
+    gameScreen.appendChild(resource);
+    resource.addEventListener('click', () => { 
+      handleResourceClick(rsrc.id);
+    });
+  })
+});
+
+socket.on('delete resources', resourceId => {
+  const resourceDiv = document.querySelector(`#${resourceId}`);
+  resourceDiv.remove();
+
+});
